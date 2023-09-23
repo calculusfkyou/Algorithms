46275590e013f0b330577b8a817026d17db0972d
calculusfkyou <114671920+calculusfkyou@users.noreply.github.com>
2023-09-23 10:21:11 +0800

    idk not yet

---
+++ 111ALGOME2/PI.py
@@ -0,0 +1,13 @@
+check=0
+while True:
+    try:
+        if check:
+            print()
+        else:
+            check=1
+        n=int(input())
+        if n%7==0:
+            print("Hello World!")
+        elif (n%7)
+    except EOFError:
+        break
